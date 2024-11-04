51ffc38cedfd1d4408244ce801183a546cdc9059
rafay183 <rafayshaikh633@gmail.com>
2024-11-04 16:45:44 +0500

    python decisions program

---
+++ 6.py
@@ -0,0 +1,16 @@
+print("Pet suggestion")
+size=input("Tell me the size you want for a pet? (small/large):").lower()
+activity=input("Which kind of activity do you like? (playful/calm):").lower()
+if size=="small" and activity=="playful" and :
+    pet="A playful kitten 🐱"
+elif size=="small" and activity=="calm":
+    pet="A calm hamster 🐹"
+elif size=="large" and activity=="playful":
+    pet="A playful Dog 🐶"
+elif size=="large" and activity=="calm":
+    pet="A calm Cow"  
+else:
+    pet="A low maintainance Fish is a best suggestion for you"
+    
+print(pet)    
+    
